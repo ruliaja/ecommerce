cc2c8e199d3b4699fc3733281ae522bc03054b84
Ruli <ruli@gmail.com>
2026-07-04 09:55:15 +0700

    modif

--- frontend/src/api/authService.js
+++ frontend/src/api/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://203.194.113.131/api';
+const API_URL = 'http://203.194.113.131/api/';
 
 // Register User
 export const registerUser = async (userData) => {
